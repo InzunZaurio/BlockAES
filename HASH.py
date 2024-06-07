57e68ddd6e3b069aac0745c2f12a3428036ba235
InzunZaurio <p.inzunza800@gmail.com>
2024-06-07 12:22:23 -0600

    Modificaciones - Creación de HASH

---
+++ HASH.py
@@ -0,0 +1,6 @@
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
+import os
+import base64
+
