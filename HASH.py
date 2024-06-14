ce37f6808e2a77367eda1d63a86fe18c292f40f5
InzunZaurio <p.inzunza800@gmail.com>
2024-06-14 09:56:44 -0600

    Orden a nueva carpeta

--- HASH.py
+++
@@ -1,6 +0,0 @@
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives import padding
-from cryptography.hazmat.backends import default_backend
-import os
-import base64
-
